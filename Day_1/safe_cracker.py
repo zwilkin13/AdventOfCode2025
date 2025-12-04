f60f31fa896df295de3e9d0cce49fba10c314fd6
Zach Wilkin <ZWilkin@roomstogo.com>
2025-12-04 14:28:31 -0500

    utilization

--- Day_1/safe_cracker.py
+++ Day_1/safe_cracker.py
@@ -10,12 +10,11 @@ def crack_that_fn_safe_man(input_file):
         for turn in turns:
             if turn[0] == 'R':  dial += int(turn[1:])   # Right Turn
             else:               dial -= int(turn[1:])   # Left Turn
-            while dial < 0:     dial = 100 - abs(dial)
-            while dial > 99:    dial -= 100
+            while not (0 <= dial <= 99):
+                dial = 100 - abs(dial) if dial < 0 else dial - 100
             if dial == 0:       zero_count += 1
 
     print(f"Cracked that mf! - {zero_count}")
-    print (zero_count)
 ...
 
 file_path = load_file_path('text_input.txt')
